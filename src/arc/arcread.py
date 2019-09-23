a77d305d1e147bc5cb7370fdb30b64db49f723d2
Syed Hussain Ather <shussainather@gmail.com>
2019-09-23 15:08:47 -0700

    REad it

--- src/arc/arcread.py
+++ src/arc/arcread.py
@@ -7,4 +7,10 @@ arcpy.env.workspace = ws = ""
 out = "output" 
 
 for cov in arcpy.ListFiles("data/arc/*.e00"): 
-    arcpy.ImportFromE00_conversion(cov, ws, cov.split('.')[0]) 
+   arcpy.ImportFromE00_conversion(cov, ws, cov.split(".")[0])
+   if not ".e00" in cov and cov != "info": 
+       arcpy.env.workspace = os.path.join(ws,cov) 
+       for fc in arcpy.ListFeatureClasses("*polygon"):
+           shp = os.path.join(out, "%s.shp" %cov) 
+           arcpy.FeatureClassToFeatureClass_conversion(fc, out) 
+ 
