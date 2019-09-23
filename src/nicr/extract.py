87385998d1ee040da162c82975c7686f8994dcd4
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 21:42:45 -0700

    read in the fie as df

--- src/nicr/extract.py
+++ src/nicr/extract.py
@@ -4,3 +4,6 @@ import pandas as pd
 Extract information and draw trends from platinum-group elements (PGE)
 with nickel and chromium mineralization.
 """
+
+df = pd.read_csv("data/nicr/nicrpge.txt", sep="|")
+print(df.head()) 
