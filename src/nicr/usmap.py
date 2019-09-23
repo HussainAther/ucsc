b40a99669272a211eae86df2d9383ac1a6903336
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 23:20:23 -0700

    map

--- src/nicr/usmap.py
+++ src/nicr/usmap.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+# Map map map!
 from mpl_toolkits.basemap import Basemap 
 from matplotlib.colors import rgb2hex
 from matplotlib.patches import Polygon
