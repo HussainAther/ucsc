74d0028b6204260a06ed8a19acd54a6f0ebdc34b
Syed Hussain Ather <shussainather@gmail.com>
2019-09-23 15:10:33 -0700

    init

---
+++ src/nicr/misc.py
@@ -0,0 +1,19 @@
+# Import numpy for number manipulation
+import numpy as np
+
+# Import pandas for data manipulation
+import pandas as pd
+
+# Map plotting tools
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+"""
+Extract information and draw trends from platinum-group elements (PGE)
+with nickel and chromium mineralization.
+"""
+
+# Read the .txt file as a pandas DataFrame df
+df = pd.read_csv("data/nicr/nicrpge.txt", sep="|", encoding = "ISO-8859-1")
+
+
