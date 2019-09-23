1034debafa419560003c7b5e96364b93e470e505
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 23:23:13 -0700

    inito

---
+++ src/nicr/table.py
@@ -0,0 +1,14 @@
+# Import numpy for number manipulation
+import numpy as np
+
+# Import pandas for data manipulation
+import pandas as pd
+
+# Map plotting tools
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+# Map map map!
+from mpl_toolkits.basemap import Basemap 
+from matplotlib.colors import rgb2hex
+from matplotlib.patches import Polygon
