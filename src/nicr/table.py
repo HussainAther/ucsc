5d9b429aed96ebd28db6accb5539ce7dd4057645
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 23:24:34 -0700

    read in ascii file

--- src/nicr/table.py
+++ src/nicr/table.py
@@ -12,3 +12,11 @@ import matplotlib.cm as cm
 from mpl_toolkits.basemap import Basemap 
 from matplotlib.colors import rgb2hex
 from matplotlib.patches import Polygon
+
+"""
+Extract information and draw trends from platinum-group elements (PGE)
+with nickel and chromium mineralization.
+"""
+
+# Read the .txt file as a pandas DataFrame df
+df = pd.read_csv("data/nicr/nicrpge.txt", sep="|", encoding = "ISO-8859-1")
