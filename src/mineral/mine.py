d6885678fa1a9884ae83c92540f465192641a204
Syed Hussain Ather <shussainather@gmail.com>
2019-09-23 19:31:55 -0700

    json and the arghonauts

--- src/mineral/mine.py
+++ src/mineral/mine.py
@@ -1,3 +1,4 @@
+import glob
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -6,6 +7,7 @@ import pandas as pd
 It's like Minecraft except not.
 """
 
+# Read 'em in!
 al = pd.read_csv("data/mineral/csv/Aluminum.csv")
 al2 = pd.read_csv("data/mineral/csv/Aluminum2.csv")
 sb = pd.read_csv("data/mineral/csv/Antimony.csv")
@@ -29,18 +31,18 @@ cu4 = pd.read_csv("data/mineral/csv/Copper4.csv")
 fel = pd.read_csv("data/mineral/csv/Feldspar.csv")
 fmo = pd.read_csv("data/mineral/csv/Ferromolybdenum.csv")
 fni = pd.read_csv("data/mineral/csv/Ferronickel.csv")
-fni = pd.read_csv("data/mineral/csv/Ferroniobium.csv")
+fno = pd.read_csv("data/mineral/csv/Ferroniobium.csv")
 fsi = pd.read_csv("data/mineral/csv/Ferrosilicon.csv")
 fv = pd.read_csv("data/mineral/csv/Ferrovanadium.csv")
 flu = pd.read_csv("data/mineral/csv/Fluorspar.csv")
 ga = pd.read_csv("data/mineral/csv/Gallium.csv")
 ge = pd.read_csv("data/mineral/csv/Germanium.csv")
 globalmined = pd.read_csv("data/mineral/csv/GlobalMined.csv")
-globalprocessed = pd.read_csv("data/mineral/csv/GlobalProcessed.csv")
+globalproc = pd.read_csv("data/mineral/csv/GlobalProcessed.csv")
 au = pd.read_csv("data/mineral/csv/Gold.csv")
 gra = pd.read_csv("data/mineral/csv/Graphite.csv")
 importmined = pd.read_csv("data/mineral/csv/ImportMined.csv")
-importproduction = pd.read_csv("data/mineral/csv/ImportProduction.csv")
+importprod = pd.read_csv("data/mineral/csv/ImportProduction.csv")
 ind = pd.read_csv("data/mineral/csv/Indium.csv")
 i = pd.read_csv("data/mineral/csv/Iodine.csv")
 fe = pd.read_csv("data/mineral/csv/Iron.csv")
@@ -69,8 +71,8 @@ po = pd.read_csv("data/mineral/csv/Phosphate.csv")
 pl = pd.read_csv("data/mineral/csv/Platinum.csv")
 pot = pd.read_csv("data/mineral/csv/Potash.csv")
 prodmined = pd.read_csv("data/mineral/csv/ProductionMined.csv")
-produproc = pd.read_csv("data/mineral/csv/ProductionProcessed.csv")
-ree= pd.read_csv("data/mineral/csv/REE.csv")
+prodproc = pd.read_csv("data/mineral/csv/ProductionProcessed.csv")
+ree = pd.read_csv("data/mineral/csv/REE.csv")
 rem = pd.read_csv("data/mineral/csv/Rhenium.csv")
 se = pd.read_csv("data/mineral/csv/Selenium.csv")
 si = pd.read_csv("data/mineral/csv/Silicon.csv")
@@ -91,3 +93,16 @@ v = pd.read_csv("data/mineral/csv/Vanadium.csv")
 y = pd.read_csv("data/mineral/csv/Yttrium.csv")
 zn = pd.read_csv("data/mineral/csv/Zinc.csv")
 zr = pd.read_csv("data/mineral/csv/Zirconium.csv")
+
+csvlist = [al, al2, sb, sb2, bar, bau, be, bi, bi2, b, br, cd, cr, 
+           cr2, co, co2, cu, cu2, cu3, cu4, fel, fmo, fni, fno, fsi, 
+           fv, flu, ga, ge, globalmined, globalproc, au, gra, importmined, 
+           importprod, ind, i, fe, pb, pb2, pb3, li, mgc, mgm, mn, mn2, 
+           mn3, hg, mica, mo, mon, ni, ni2, ni3, nio, nim, nb, pgm, pdm, 
+           po, pl, pot, prodmined, prodproc, ree, rem, se, si, ag, smeltsn, 
+           smeltzn, steel, sr, s, ta, te, sn, ti, w, usmined, usproc, v, y, zn, zr]
+
+o = open("data/mineral/all.json", "w")
+
+for csv in csvlist:
+    o.write(pd.DataFrame.to_json(csv))
