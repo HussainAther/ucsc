037ccfaaebd07c248856d8a9d51144009fd70655
Syed Hussain Ather <shussainather@gmail.com>
2019-09-23 18:46:55 -0700

    moar

--- src/mineral/mine.py
+++ src/mineral/mine.py
@@ -7,3 +7,7 @@ It's like Minecraft except not.
 """
 
 al1 = pd.read_csv("data/mineral/csv/Aluminum1.csv")
+al2 = pd.read_csv("data/mineral/csv/Aluminum2.csv")
+sb1 = pd.read_csv("data/mineral/csv/Antimony1.csv")
+sb2 = pd.read_csv("data/mineral/csv/Antimony2.csv")
+barite = pd.read_csv("data/mineral/csv/Barite.csv")
