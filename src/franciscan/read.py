9e0a4f62dee3df741000ab467a0ac080803d50e6
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 15:05:56 -0700

    init

---
+++ src/franciscan/read.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+import os
+
+# Reading the json as a dict
+with open("data/franciscan/franciscan.json") as json_data:
+    data = json.load(json_data, encoding="utf-8")
+
+
