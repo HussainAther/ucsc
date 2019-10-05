6862db6b05b981b5ca96a16977c20e2ea43c6bd1
Syed Hussain Ather <shussainather@gmail.com>
2019-10-05 00:21:27 -0700

    column encoding

--- src/santacruz/csvread.py
+++ src/santacruz/csvread.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index="fid")
-magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index="fid")
+raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index_col="fid", encoding="utf-8")
+magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index_col="fid", encoding="utf-8")
 
+print(magdf.columns)
