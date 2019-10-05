e2b81a7aed3b3ff512fd86229a68bf13e846b7c2
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 17:08:56 -0700

    reading

--- src/mineral/shapefileread.py
+++ src/mineral/shapefileread.py
@@ -2,7 +2,6 @@ import shapefile
 import numpy as np
 
 import matplotlib
-matplotlib.style.use('ggplot')
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
@@ -10,4 +9,7 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from matplotlib.cm import *
 from IPython.display import Image as ImageShow
+
+sf = shapefile.Reader("data/mineral/mineplant-fUS/mineplant-fUS.shp")
+shapes  = sf.shapes()
 
