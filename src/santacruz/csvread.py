96366137b558eaa35fecdcf7c2b9fd72238c2970
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 23:33:49 -0700

    index

--- src/santacruz/csvread.py
+++ src/santacruz/csvread.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv")
-magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv")
+raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index="fid")
+magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index="fid")
+
