34b707cfe2bb71662601e545a3424c559d83a7e0
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 17:31:46 -0700

    geo

--- src/mineral/shapefileread.py
+++ src/mineral/shapefileread.py
@@ -1,16 +1,19 @@
 import shapefile
 import numpy as np
-import geopandas s gpd
+import geopandas as gpd
 
 import matplotlib
 import matplotlib.pyplot as plt
 
-fp = "data/mineral/mineplant-fUS/mineplant-fUS.shp")
+fp = "data/mineral/mineplant-fUS/mineplant-fUS.shp"
 
-# sf = shapefile.Reader("data/mineral/mineplant-fUS/mineplant-fUS.shp")
+# sf = shapefile.Reader(fp)
 # shapes  = sf.shapes()
 # feature = sf.shapeRecords()[0]
 # first = feature.shape.__geo_interface__ 
 # print(first)
 
 data = gpd.read_file(fp) 
+type(data)
+print(data.head())
+data.plot()
