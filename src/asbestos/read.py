2d4dbd57a35edc3fdf306da315a4213a10b64d1a
Syed Hussain Ather <shussainather@gmail.com>
2019-10-05 18:51:11 -0700

    printing ca

--- src/asbestos/read.py
+++ src/asbestos/read.py
@@ -8,11 +8,15 @@ from shapely.geometry import Point
 Let's get down to business to defeat asbestos.
 """
 
+# Read data.
 df = pd.read_csv("data/asbestos/main.csv")
 
+# Extract data.
 geom = [Point(xy) for xy in zip(df["longitude"], df["latitude"])]
 gdf = gpd.GeoDataFrame(
     df, geometry=geom)
+
+# Save to file.
 crs = {"init": "epsg:2263"} #http://www.spatialreference.org/ref/epsg/2263/
 gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geom)
 gdf.to_file(driver="ESRI Shapefile", filename="output/asbestos/data")
@@ -27,3 +31,21 @@ gdf.plot(ax=ax, color="red")
 
 # Save.
 plt.savefig("output/asbestos/usmap.png")
+
+# Close.
+plt.close()
+
+# Restrict to California.
+ca = country[country["NAME"].isin(["California"]) == True]
+df = df.loc[df["state"] == "CA"]
+geom = [Point(xy) for xy in zip(df["longitude"], df["latitude"])]
+gdf = gpd.GeoDataFrame(
+    df, geometry=geom)
+
+# Plot.
+ax = ca.plot(color="white", edgecolor="black")
+gdf.plot(ax=ax, color="red")
+
+# Save.
+plt.savefig("output/asbestos/california.png")
+
