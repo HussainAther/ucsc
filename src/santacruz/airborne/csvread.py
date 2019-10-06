01f06e2a1e5110094340b4309da27e98964aa9d7
Syed Hussain Ather <shussainather@gmail.com>
2019-10-06 14:10:15 -0700

    defs

--- src/santacruz/airborne/csvread.py
+++ src/santacruz/airborne/csvread.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -18,3 +19,30 @@ def valpoint(p):
     lat, lon = p
     assert -90 <= lat <= 90, "bad latitude"
     assert -180 <= lon <= 180, "bad longitude"
+
+def distance_haversine(p1, p2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    Haversine
+    formula: 
+        a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+        c = 2 ⋅ atan2( √a, √(1−a) )
+        d = R ⋅ c
+    where   φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
+            note that angles need to be in radians to pass to trig functions!
+    """
+    lat1, lon1 = p1
+    lat2, lon2 = p2
+    for p in [p1, p2]:
+        validate_point(p)
+    R = 6371 # km - earths's radius
+    # convert decimal degrees to radians 
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    # haversine formula 
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.asin(np.sqrt(a)) # 2 * np.atan2(np.sqrt(a), np.sqrt(1-a))
+    d = R * c
+    return d
