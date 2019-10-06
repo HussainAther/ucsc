aac69e42de25095dbecbf74cda8569aa383dcce7
Syed Hussain Ather <shussainather@gmail.com>
2019-10-05 22:07:27 -0700

    us only

--- src/rock/read.py
+++ src/rock/read.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from geopandas.tools import sjoin
 from shapely import wkt
 
 """
@@ -19,13 +20,18 @@ gdf.plot(ax=ax, color="red", markersize=5)
 plt.savefig("output/rock/worldmap.png")
 plt.close()
 
+plt.figure()
+
 # Restrict to America, excluding Hawaii and Alaska.
 country = gpd.read_file("data/gz_2010_us_040_00_5m.json")
-country = country[country["NAME"].isin(["Alaska","Hawaii"]) == False]
+country = country[country["NAME"].isin(["Alaska","Hawaii", "Puerto Rico"]) == False]
+usmask = gdf.within(country.loc[0, "geometry"])
+usdata = gdf.loc[usmask]
 
-# Plot.
+# Plot the US map.
 ax = country.plot(color="white", edgecolor="black")
-gdf.plot(ax=ax, color="red")
+usdata.plot(ax=ax, color="red", markersize=5)
 
 # Save.
-plt.savefig("output/asbestos/usmap.png")
+plt.savefig("output/rock/usmap.png")
+plt.close()
