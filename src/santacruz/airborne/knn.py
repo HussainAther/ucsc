6cf06800216d0be82f53672569f07b1085b96d22
Syed Hussain Ather <shussainather@gmail.com>
2019-10-06 15:25:17 -0700

    trying new stuff

--- src/santacruz/airborne/knn.py
+++ src/santacruz/airborne/knn.py
@@ -6,10 +6,6 @@ import seaborn as sns
 raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index_col="fid", encoding="utf-8")
 magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index_col="fid", encoding="utf-8")
 
-# Plot.
-plot = sns.lmplot("latitude", "longitude", data=magdf, fit_reg=False, hue="diurnal", legend=False)
-plot.savefig("output/santacruz/quadrangle.png")
-
 # K-Nearest neighbors clustering (kNN) with
 # greater circle distance as a metric to minimize.
 # Also kd-tree (kd tree kdtree).
--- src/santacruz/airborne/svm.py
+++ src/santacruz/airborne/svm.py
@@ -3,10 +3,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from sklearn.svm import SVC 
+
 raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index_col="fid", encoding="utf-8")
 magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index_col="fid", encoding="utf-8")
-
-# Plot.
-plot = sns.lmplot("latitude", "longitude", data=magdf, fit_reg=False, hue="diurnal", legend=False)
-plot.savefig("output/santacruz/quadrangle.png")
 
