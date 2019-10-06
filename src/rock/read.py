0e35cf8548b24707c9498d0c246afae85cc96518
Syed Hussain Ather <shussainather@gmail.com>
2019-10-05 21:37:11 -0700

    mapping

--- src/rock/read.py
+++ src/rock/read.py
@@ -1,3 +1,20 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
+
+from shapely import wkt
+
+"""
+Rocks.
+"""
+
+# Read in shapefile as geopands dataframe.
+gdf = gpd.read_file("data/rock/ngdbrock.shp", encoding="utf-8")
+
+# Plot.
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+ax = world.plot(
+    color="white", edgecolor="black")
+gdf.plot(ax=ax, color="red", markersize=5)
+plt.savefig("output/rock/map.png")
+plt.close()
