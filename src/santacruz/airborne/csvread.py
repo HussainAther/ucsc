ee037594ef862bf8c4757e1060204d7417bd2d96
Syed Hussain Ather <shussainather@gmail.com>
2019-10-05 23:28:06 -0700

    test

--- src/santacruz/airborne/csvread.py
+++ src/santacruz/airborne/csvread.py
@@ -5,5 +5,8 @@ import seaborn as sns
 raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index_col="fid", encoding="utf-8")
 magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index_col="fid", encoding="utf-8")
 
+print(raddf)
+print(magdf)
+
 plot = sns.lmplot("latitude", "longitude", data=magdf, fit_reg=False, hue="diurnal", legend=False)
 plot.savefig("output/santacruz/quadrangle.png")
