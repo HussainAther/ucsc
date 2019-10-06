776447a553d4ba98423fbb4391fdff5f1a073562
Syed Hussain Ather <shussainather@gmail.com>
2019-10-06 16:50:05 -0700

    comment

--- src/santacruz/airborne/svm.py
+++ src/santacruz/airborne/svm.py
@@ -5,6 +5,19 @@ import seaborn as sns
 
 from sklearn.svm import SVC 
 
+"""
+Support vector machine (SVM svm).
+"""
+
+# Read in data.
 raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index_col="fid", encoding="utf-8")
 magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index_col="fid", encoding="utf-8")
+
+# Extract dat of interest.
+X = magdf["latitude"]
+y = magdf["longitude"]
+target = magdf["diurnal"]
+
+# And the rest of it.
+clf = SVC(kernel="rbf")
 
