16c7ac6fda0cd26b90073e21363a64ac81554f0b
Syed Hussain Ather <shussainather@gmail.com>
2019-10-05 18:20:37 -0700

    north america map

--- src/asbestos/read.py
+++ src/asbestos/read.py
@@ -1,6 +1,22 @@
 import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
 
-fp = "data/santacruz/asbestos/asbestos.shp"
+from shapely.geometry import Point
 
-data = gpd.read_file(fp)
-print(data.head())
+"""
+Let's get down to business to defeat asbestos.
+"""
+
+df = pd.read_csv("data/asbestos/main.csv")
+
+fig, ax = plt.subplots(figsize=(100, 100))
+geometry = [Point(xy) for xy in zip(df["latitude"], df["longitude"])]
+crs = {"init": "epsg:2263"} #http://www.spatialreference.org/ref/epsg/2263/
+gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+gdf.to_file(driver="ESRI Shapefile", filename="output/asbestos/data")
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+
+# We restrict to North America.
+ax = world[world.continent == "North America"].plot(
+    color="white", edgecolor='black')
