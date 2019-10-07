78cf5a5cfaf44ada8dd18b3078e5c6561eb3d927
Syed Hussain Ather <shussainather@gmail.com>
2019-10-06 19:52:07 -0700

    preprocess

--- src/santacruz/airborne/lr.py
+++ src/santacruz/airborne/lr.py
@@ -1,9 +1,9 @@
-import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -16,7 +16,11 @@ raddf = pd.read_csv("data/santacruz/airborne/santa_cruz_rad.csv", index_col="fid
 magdf = pd.read_csv("data/santacruz/airborne/santa_cruz_mag.csv", index_col="fid", encoding="utf-8")
 
 # Extract data of interest for the lr.
-X = [list(a) for a in zip(magdf["latitude"], magdf["longitude"])] # Features we test
-y = list(magdf["diurnal"])
-target = ynames
-feature = ["latitude", "longitude"] # Feature names
+X = [list(a) for a in zip(magdf["latitude"], magdf["longitude"])] # features we test
+lab_enc = preprocessing.LabelEncoder() # label encoder
+y = magdf["diurnal"].values
+y = lab_enc.fit_transform(y)
+feature = ["latitude", "longitude"] # feature names
+
+clf = LogisticRegression(random_state=0, solver="lbfgs",
+    multi_class="multinomial").fit(X, y)
