cbc096b6559ac6daec57542ad273ec3cb7e3886a
Syed Hussain Ather <shussainather@gmail.com>
2019-10-06 19:58:29 -0700

    plots

--- src/santacruz/airborne/lr.py
+++ src/santacruz/airborne/lr.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import classification_report,confusion_matrix, mean_squared_error, r2_score
 
 """
 Logistic regression (lr)
@@ -22,5 +22,112 @@ y = magdf["diurnal"].values
 y = lab_enc.fit_transform(y)
 feature = ["latitude", "longitude"] # feature names
 
+# Perform logistic regression.
 clf = LogisticRegression(random_state=0, solver="lbfgs",
     multi_class="multinomial", max_iter=1000).fit(X, y)
+
+# Extract probabilities.
+xx, yy = np.mgrid[-5:5:.01, -5:5:.01]
+grid = np.c_[xx.ravel(), yy.ravel()]
+probs = clf.predict_proba(grid)[:, 1].reshape(xx.shape)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title="Confusion matrix",
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    fig = plt.figure(figsize=(10, 8)) 
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+    plt.savefig("output/santacruz/airborne/confusion.png")
+    plt.close()
+
+# probability grid
+f, ax = plt.subplots(figsize=(8, 6))
+contour = ax.contourf(xx, yy, probs, 25, cmap="RdBu",
+                      vmin=0, vmax=1)
+ax_c = f.colorbar(contour)
+ax_c.set_label("$P(y = 1)$")
+ax_c.set_ticks([0, .25, .5, .75, 1])
+
+ax.scatter(X[100:,0], X[100:, 1], c=y[100:], s=50,
+           cmap="RdBu", vmin=-.2, vmax=1.2,
+           edgecolor="white", linewidth=1)
+
+ax.set(aspect="equal",
+       xlim=(-5, 5), ylim=(-5, 5),
+       xlabel="$X_1$", ylabel="$X_2$")
+plt.savefig("output/santacruz/airborne/probgrid.png")
+
+# decision boundary
+f, ax = plt.subplots(figsize=(8, 6))
+ax.contour(xx, yy, probs, levels=[.5], cmap="Greys", vmin=0, vmax=.6)
+
+ax.scatter(X[100:,0], X[100:, 1], c=y[100:], s=50,
+           cmap="RdBu", vmin=-.2, vmax=1.2,
+           edgecolor="white", linewidth=1)
+
+ax.set(aspect="equal",
+       xlim=(-5, 5), ylim=(-5, 5),
+       xlabel="$X_1$", ylabel="$X_2$")
+plt.savefig("output/santacruz/airaborne/decisionboundary.png")
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title="Confusion matrix",
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    fig = plt.figure(figsize=(10, 8)) 
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+    plt.savefig("output/santacruz/airborne/confusion.png")
+    plt.close()
+
+# Predict results from the test data.
+predicted = clf.predict(X)
+
+# Plot the confusion matrix.
+cm = confusion_matrix(y,predicted)
+plot_confusion_matrix(cm, classes=target,
+                      title="Confusion matrix, without normalization")
+
