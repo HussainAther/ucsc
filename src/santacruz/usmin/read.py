a7ecc5e0ea069d7e01ffe034f785251e10d3621c
Syed Hussain Ather <shussainather@gmail.com>
2019-10-06 21:52:01 -0700

    plot

--- src/santacruz/usmin/read.py
+++ src/santacruz/usmin/read.py
@@ -15,6 +15,31 @@ from matplotlib.patches import Polygon
 fp = "data/santacruz/usmin/poly.shp"
 
 gdf = gpd.read_file(fp) 
-print(gdf.columns)
-print(gdf.head())
-sys.exit()
+
+# Restrict to America.
+country = gpd.read_file("data/gz_2010_us_040_00_5m.json")
+country = country[country["NAME"].isin(["Alaska","Hawaii"]) == False]
+
+# Plot.
+ax = country.plot(color="white", edgecolor="black")
+gdf.plot(ax=ax, color="red")
+
+# Save.
+plt.savefig("output/santacruz/usmin/usmap.png")
+
+# Close.
+plt.close()
+
+# Restrict to California.
+ca = country[country["NAME"].isin(["California"]) == True]
+gdf = gdf.loc[gdf["state"] == "CA"]
+geom = [Point(x) for x in gdf["geometry"]]
+gdf = gpd.GeoDataFrame(
+    gdf, geometry=geom)
+
+# Plot.
+ax = ca.plot(color="white", edgecolor="black")
+gdf.plot(ax=ax, color="red")
+
+# Save.
+plt.savefig("output/usmin/california.png")
