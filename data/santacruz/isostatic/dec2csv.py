885ba333a7a20429fc37dadd1e6081fec81edb24
Syed Hussain Ather <shussainather@gmail.com>
2019-10-08 00:08:01 -0700

    workin thru

--- data/santacruz/isostatic/dec2csv.py
+++ data/santacruz/isostatic/dec2csv.py
@@ -3,4 +3,14 @@ o = open("data/santacruz/isostatic/iso.csv", "w")
 linelist = []
 with open("data/santacruz/isostatic/iso.dec", "r") as file:
     for line in file:
-        if line.split() 
+        if line.split()[0] == "ORD" or line.split()[0] == "SCR":
+            if len(line.split()) == 15:
+                linelist.append([line.split()[0] + line.split()[1]] + line.split()[2:])
+            elif len(line.split()) == 14: 
+                linelist.append([line.split()[0] + line.split()[1]] + line.split()[2:-2] + [""] + line.split()[-2:])
+        elif len(line.split()) == 15:
+            linelist.append(line.split()[:7] + line.split()[8:])
+        elif len(line.split()) == 14:
+            linelist.append(line.split())
+        elif len(line.split()) == 13:
+            print(line.split()) 
