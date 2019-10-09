8651a1dff088222210bbb45e6221f4b44856c8c3
Syed Hussain Ather <shussainather@gmail.com>
2019-10-08 20:44:13 -0700

    init

--- src/mineral/specificmine.py
+++ src/mineral/specificmine.py
@@ -7,7 +7,7 @@ I wish it were Minecraft.
 """
 
 # Read 'em in!
-bau = pd.read_csv("data/mineral/csv/Bauxite.csv")
+fe = pd.read_csv("data/mineral/csv/Iron.csv")
 cu = pd.read_csv("data/mineral/csv/Copper.csv")
 cu2 = pd.read_csv("data/mineral/csv/Copper2.csv")
 cu3 = pd.read_csv("data/mineral/csv/Copper3.csv")
@@ -15,7 +15,7 @@ cu4 = pd.read_csv("data/mineral/csv/Copper4.csv")
 mo = pd.read_csv("data/mineral/csv/Molybdenum.csv")
 ta = pd.read_csv("data/mineral/csv/Tantalum.csv")
 
-alldf = [bau, cu, cu2, cu3, cu4, mo, ta]
+alldf = [fe, cu, cu2, cu3, cu4, mo, ta]
 
 outjson = open("output/mineral/specificall.json", "w")
 outhtml = open("output/mineral/specificall.html", "w")
@@ -28,7 +28,7 @@ for df in alldf:
 
 usmined = pd.read_csv("data/mineral/csv/USMined.csv")
 
-usminedconsumption = pd.concat([usmined.loc[usmined.Value == "BauxiteConsumption"],
+usminedconsumption = pd.concat([usmined.loc[usmined.Value == "IronConsumption"],
                            usmined.loc[usmined.Value == "CopperConsumption"],
                            usmined.loc[usmined.Value == "MolybdenumConsumption"],
                            usmined.loc[usmined.Value == "TantalumConsumption"]])
@@ -37,12 +37,12 @@ usminedconsumption.columns = usminedconsumption.iloc[0]
 usminedconsumption = usminedconsumption[1:]
 
 # Plot.
-plt.scatter(["2005", "2006", "2007", "2008", "2009"],
-             list(usminedconsumption["BauxiteConsumption"].values)) 
-plt.scatter(["2005", "2006", "2007", "2008", "2009"],
+plt.plot(["2005", "2006", "2007", "2008", "2009"],
+             list(usminedconsumption["IronConsumption"].values)) 
+plt.plot(["2005", "2006", "2007", "2008", "2009"],
              list(usminedconsumption["CopperConsumption"].values)) 
-plt.scatter(["2005", "2006", "2007", "2008", "2009"],
+plt.plot(["2005", "2006", "2007", "2008", "2009"],
              list(usminedconsumption["MolybdenumConsumption"].values)) 
-plt.scatter(["2005", "2006", "2007", "2008", "2009"],
+plt.plot(["2005", "2006", "2007", "2008", "2009"],
              list(usminedconsumption["TantalumConsumption"].values))
 plt.savefig("output/mineral/specificgraph.png") 
