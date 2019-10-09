fdc7bc574b9747e33082d98f9d69ff9c1eb6d8b0
Syed Hussain Ather <shussainather@gmail.com>
2019-10-09 15:44:50 -0700

    changed max iter

--- src/santacruz/airborne/svm.py
+++ src/santacruz/airborne/svm.py
@@ -34,7 +34,7 @@ feature = ["latitude", "longitude"] # Feature names
 # Let's get machine learning in this joint. 
 clf = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
     decision_function_shape=None, degree=3, gamma="auto", kernel="rbf",
-    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    max_iter=100, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
 
 # Fit.
