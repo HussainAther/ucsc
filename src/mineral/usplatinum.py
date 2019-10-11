8f81ea09693f7daf4e68134871540e4f8e5d7a29
Syed Hussain Ather <shussainather@gmail.com>
2019-10-11 01:47:56 -0700

    world graph

--- src/mineral/usplatinum.py
+++ src/mineral/usplatinum.py
@@ -14,3 +14,13 @@ usproc = usproc.transpose()
 
 pt["Year"] = [int(i) for i in pt["Year"]]
 usproc["Year"] = [int(i) for i in usproc.index]
+
+fig = plt.figre()
+ax = fig.add_subplot()
+plt.plot("Year", "Worldmineproduction", data=pt)
+plt.title("World Platinum Production")
+plt.ylabel("Thousand Metric Tons")
+plt.xlabel("Year")
+plt.xticks(range(1990, 2010)) 
+plt.savefig("output/mineral/ptworldprod.png")
+
