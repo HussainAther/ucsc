e2978734ed971f05e63db4b0c2ff8e338a64cf99
Syed Hussain Ather <shussainather@gmail.com>
2019-10-11 01:10:56 -0700

    year

--- src/mineral/usmanganese.py
+++ src/mineral/usmanganese.py
@@ -10,5 +10,14 @@ I wish it were Minecraft.
 mn = pd.read_csv("data/mineral/csv/Manganese.csv")
 fmn = pd.read_csv("data/mineral/csv/Ferromanganese.csv")
 smn = pd.read_csv("data/mineral/csv/Silicomanganese.csv")
-ismine = pd.read_csv("data/mineral/csv/USMined.csv", index_col="Value")
+usmine = pd.read_csv("data/mineral/csv/USMined.csv", index_col="Value")
 usproc = pd.read_csv("data/mineral/csv/USProcessed.csv", index_col="Value")
+
+usmine = usmine.transpose()
+usproc = usproc.transpose()
+
+mn["Year"] = [int(i) for i in mn["Year"]]
+fmn["Year"] = [int(i) for i in fmn["Year"]]
+smn["Year"] = [int(i) for i in smn["Year"]]
+usmine["Year"] = [int(i) for i in usmine.index]
+usproc["Year"] = [int(i) for i in usproc.index]
