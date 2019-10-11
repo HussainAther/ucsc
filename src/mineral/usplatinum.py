405abb3e9beb818eb71b6d331f87119140a3bbaa
Syed Hussain Ather <shussainather@gmail.com>
2019-10-11 01:53:10 -0700

    pro and con

--- src/mineral/usplatinum.py
+++ src/mineral/usplatinum.py
@@ -15,7 +15,7 @@ usproc = usproc.transpose()
 pt["Year"] = [int(i) for i in pt["Year"]]
 usproc["Year"] = [int(i) for i in usproc.index]
 
-fig = plt.figre()
+fig = plt.figure()
 ax = fig.add_subplot()
 plt.plot("Year", "Worldmineproduction", data=pt)
 plt.title("World Platinum Production")
@@ -24,3 +24,13 @@ plt.xlabel("Year")
 plt.xticks(range(1990, 2010)) 
 plt.savefig("output/mineral/ptworldprod.png")
 
+fig = plt.figure()
+ax = fig.add_subplot()
+# Palladium Consumption in thousand metric ton
+usproc["pgepdcon"] = [float(i)*0.000001 for i in usproc["PlatinumGroupMetals(kg)ConsumptionPalladium"]]
+# Platinum Consumption in thousand metric ton
+usproc["pgeptcon"] = [float(i)*0.000001 for i in usproc["PlatinumGroupMetals(kg)ConsumptionPlatinum"]]
+# Palladium Production in thousand metric ton
+usproc["pgeptpro"] = [float(i)*0.000001 for i in usproc["PlatinumGroupMetals(kg)ProductionPalladium"]]
+# Platinum Production in thousand metric ton
+usproc["pgepdpro"] = [float(i)*0.000001 for i in usproc["PlatinumGroupMetals(kg)ProductionPlatinum"]]
