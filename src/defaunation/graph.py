f272e2ef861e807ed143772ee00eea8e1f727f59
Syed Hussain Ather <shussainather@gmail.com>
2019-11-01 03:25:11 -0700

    indiv graphs

--- src/defaunation/graph.py
+++ src/defaunation/graph.py
@@ -8,4 +8,20 @@ Defaunation statistics.
 
 df = pd.DataFrame.from_csv("data/defaunation/gardneretal.csv", encoding="windows-1252")
 
-print(df["Region"].value_counts())
+colors = ["b", "g", "r", "c", "m", "y", "k"]
+
+# plt.figure(figsize=(4,15))
+plt.figure()
+df["Region"].value_counts().plot.bar(stacked=True)
+plt.xlabel("Region")
+plt.ylabel("Count")
+plt.tight_layout()
+plt.savefig("output/defaunation/region.png") 
+
+# plt.figure(figsize=(10,4))
+plt.figure()
+df["Study design"].value_counts().plot.barh(stacked=True)
+plt.ylabel("Study design")
+plt.xlabel("Count")
+plt.tight_layout()
+plt.savefig("output/defaunation/studydesign.png") 
