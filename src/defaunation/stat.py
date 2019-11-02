7365f1f4162ea6637af3e3f13af4f623eccfd024
Syed Hussain Ather <shussainather@gmail.com>
2019-11-02 13:18:27 -0700

    init

---
+++ src/defaunation/stat.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+"""
+Defaunation statistics.
+"""
+
+df = pd.DataFrame.from_csv("data/defaunation/gardneretal.csv", encoding="windows-1252")
+
+print(df)
