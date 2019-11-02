471a02d1d5eae0615239d993dfc73cf9002cbf6d
Syed Hussain Ather <shussainather@gmail.com>
2019-11-02 15:43:14 -0700

    reading shapefile

--- src/criticalminerals/graph.py
+++ src/criticalminerals/graph.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import shapefile
 
 """
 Critical mineral graphs
 """
+
+shape = shapefile.Reader("data/criticalminerals/PP1802_CritMin_pts.shp") 
