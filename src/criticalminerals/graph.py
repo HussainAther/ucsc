933e64bb6274fcc80c203c85ce93f0ce7e659036
Syed Hussain Ather <shussainather@gmail.com>
2019-11-02 16:13:16 -0700

    init

--- src/criticalminerals/graph.py
+++ src/criticalminerals/graph.py
@@ -1,3 +1,5 @@
+import geopandas as gpd
+import geoplot
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,9 +9,17 @@ import shapefile
 Critical mineral graphs
 """
 
-shape = shapefile.Reader("data/criticalminerals/PP1802_CritMin_pts.shp") 
+# shape = shapefile.Reader("data/criticalminerals/PP1802_CritMin_pts.shp") 
 
-#first feature of the shapefile
-feature = shape.shapeRecords()[0]
-first = feature.shape.__geo_interface__  
-print(first) # (GeoJSON format)
+# # First feature of the shapefile
+# feature = shape.shapeRecords()[0]
+# first = feature.shape.__geo_interface__  
+# print(first) # (GeoJSON format)
+
+gdf = gpd.read_file("data/criticalminerals/PP1802_CritMin_pts.shp")
+
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+geoplot.polyplot(world, figsize=(8, 4))
+ax = world.plot(color="white", edgecolor="black")
+gdf.plot(ax=ax, color="red")
+plt.savefig("output/criticalminerals/worldmap.png")
