049703a8eeb45c1d10c0d0c3a7b27626d4da33a8
Syed Hussain Ather <shussainather@gmail.com>
2019-11-02 13:34:28 -0700

    nvm

--- src/defaunation/stat.py
+++ src/defaunation/stat.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import researchpy
 
 """
 Defaunation statistics.
 """
 
+# Tranpose the input DataFrame.
 df = pd.DataFrame.from_csv("data/defaunation/gardneretal.csv", encoding="windows-1252")
 
-print(df)
+print(df.head())
+print(df.loc[df["Region"] == "Neotropics"])
+print(df.columns)
+# researchpy.ttest(df.loc[df["Region"] == "Neotropics"], df.loc[df["Region"] == "Africa"])
+
