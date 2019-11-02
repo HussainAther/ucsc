5a37d657f9a2666997c0917d4400b079aeebc524
Syed Hussain Ather <shussainather@gmail.com>
2019-11-02 12:57:12 -0700

    moar

--- src/defaunation/graph.py
+++ src/defaunation/graph.py
@@ -10,11 +10,12 @@ df = pd.DataFrame.from_csv("data/defaunation/gardneretal.csv", encoding="windows
 
 colors = ["b", "g", "r", "c", "m", "y", "k"]
 
-# plt.figure(figsize=(4,15))
+# plt.figure(figsize=(4,23))
 plt.figure()
 df["Region"].value_counts().plot.bar(stacked=True)
 plt.xlabel("Region")
 plt.ylabel("Count")
+plt.xticks(rotation=45)
 plt.tight_layout()
 plt.savefig("output/defaunation/region.png") 
 
@@ -26,4 +27,9 @@ plt.xlabel("Count")
 plt.tight_layout()
 plt.savefig("output/defaunation/studydesign.png")
 
-
+plt.figure()
+df["Fauna"].value_counts().plot.barh(stacked=True)
+plt.ylabel("Fauna")
+plt.xlabel("Count")
+plt.tight_layout()
+plt.savefig("output/defaunation/fauna.png")
